a5368fdc01815ab65f3cbbb7ce4424b94f2a62c7
Ederson João <ederson.joao@outlook.com>
2026-01-21 09:43:20 -0300

    cadastro de usuário no banco

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
 import { z } from "zod"
-
+import { hash } from "bcrypt";
+import { AppError } from "@/utils/AppError";
 
 class UserController {
-    create(request: Request, response: Response){
+   async create(request: Request, response: Response){
         const bodySchema = z.object({
             name: z.string().trim().min(1),
             email: z.string().email(),
@@ -12,7 +14,25 @@ class UserController {
 
         const { name, email, password } = bodySchema.parse(request.body)
 
-        return response.json({ message: "ok"})
+        const userWithSameEmail = await prisma.user.findFirstOrThrow({ where: { email }})
+
+        if (userWithSameEmail) {
+            throw new AppError("User with same email already exists")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            },
+        })
+
+        const { password: _, ...userWithoutPassword } = user
+
+        return response.status(201).json(userWithoutPassword)
     }
 }
 
