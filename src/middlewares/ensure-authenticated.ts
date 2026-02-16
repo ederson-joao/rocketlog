e15825ed5473cfdad3d3988d156d6ff80a1ec6cd
Ederson João <ederson.joao@outlook.com>
2026-02-16 10:14:06 -0300

    autorização e autenticação

--- src/middlewares/ensure-authenticated.ts
+++ src/middlewares/ensure-authenticated.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 import { authConfig } from "@/configs/auth";
 import { AppError } from "@/utils/AppError";
+import exp from "constants";
 
 interface TokenPayload {
     role: string
@@ -29,3 +30,5 @@ function ensureAuthenticated(request: Request, response: Response, next: NextFun
         throw new AppError("Invalid JWT token", 401)
     }
 }
+
+export { ensureAuthenticated }
